fca0a759fc76b465538106c1a6cd43a74601e4f3
Salil.Kaul <salilk@intelligrape.com>
2017-05-11 21:05:03 +0530

    First Commit

---
+++ app/src/main/java/colormemory/com/colormemory/MainActivity.java
@@ -0,0 +1,16 @@
+package colormemory.com.colormemory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.tl);
+        //tableLayout.getRotationX()
+    }
+}
