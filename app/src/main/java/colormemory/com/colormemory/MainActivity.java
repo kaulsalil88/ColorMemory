d2d29846897b115e2200bbfe7840417ea5bdc050
Salil.Kaul <salilk@intelligrape.com>
2017-05-12 12:19:11 +0530

    Added sample fragments .

--- app/src/main/java/colormemory/com/colormemory/MainActivity.java
+++ app/src/main/java/colormemory/com/colormemory/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TableLayout tableLayout = (TableLayout) findViewById(R.id.tl);
+        //TableLayout tableLayout = (TableLayout) findViewById(R.id.tl);
         ////
         // tableLayout.getRotationX()//
     //
