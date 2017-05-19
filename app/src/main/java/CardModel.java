79b082208ae4a0ca38f575ffcb308881f06ec596
Salil.Kaul <salilk@intelligrape.com>
2017-05-19 19:43:02 +0530

    Added resources and code to display .

---
+++ app/src/main/java/CardModel.java
@@ -0,0 +1,11 @@
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by salil-kaul on 19/5/17.
+ */
+
+public class CardModel {
+
+    int id;
+    Drawable drawable;
+}
--- app/src/main/java/colormemory/com/colormemory/MainActivity.java
+++ app/src/main/java/colormemory/com/colormemory/MainActivity.java
@@ -1,21 +1,38 @@
 package colormemory.com.colormemory;
 
+import android.app.Activity;
+import android.databinding.DataBindingComponent;
+import android.databinding.DataBindingUtil;
+import android.media.Image;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 
+import colormemory.com.colormemory.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int mCurrentScore = 0;
+    boolean mstrategy = false;
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //TableLayout tableLayout = (TableLayout) findViewById(R.id.tl);
-        ////
-        // tableLayout.getRotationX()//
-        //
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        getSupportFragmentManager().beginTransaction().
-                add(R.id.frame, FragmentA.newInstance("", "")).addToBackStack("fragA").commit();
+        for (int i = 0; i < 4; i++) {
+            if (i % 2 == 0) {
+                mstrategy = true;
+            } else {
+                mstrategy = false;
+            }
+            for (int j = 0; j < 4; j++) {
+
+            }
+        }
+
     }
 }
